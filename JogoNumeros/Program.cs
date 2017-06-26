a571f1f8e3049603195639efcb59e90ab8378d5c
Dan Silva <danilo_silva1987@live.com>
2017-06-26 18:58:30 -0300

    Pronto

---
+++ JogoNumeros/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite um Número");
+            num = int.Parse(Console.ReadLine());
+            //conta a quantidade de divisíveis
+            for (int cont = 1; cont <= num/3; cont++)
+
+            if (cont !=3)
+            {
+               
+                Console.WriteLine ("Fizz");
+            }
+            for (int cont = 1; cont <= num/5; cont++)
+
+            if (cont !=5)
+            {
+                
+                Console.WriteLine("Buzz");
+            }
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
